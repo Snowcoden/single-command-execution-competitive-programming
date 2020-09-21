5736409f28e3f6d77a8153299e61628e4fba2acd
Arkajyoti Banerjee <34052538+Snowden1738@users.noreply.github.com>
2020-09-21 17:04:25 +0530

    Fixed INSTALL.sh taking in consideration that git might not be installed in the system

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,3 +1,12 @@
+git_path=`whereis git | cut -d ":" -f 2`
+
+if [ "$git_path" = "" ]; then
+    echo ""
+    echo "Please install git before running this command."
+    echo ""
+    exit 0
+fi
+
 git clone https://github.com/Snowden1738/single-command-execution-competitive-programming
 cd single-command-execution-competitive-programming
 chmod +x cpr
