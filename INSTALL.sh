6ce369da5bd0c8866610b5a5da6a35d6d45f7086
Arkajyoti Banerjee <34052538+Snowden1738@users.noreply.github.com>
2020-09-21 17:36:30 +0530

    Fixed terminal reload command

--- INSTALL.sh
+++ INSTALL.sh
@@ -12,5 +12,4 @@ cd single-command-execution-competitive-programming
 chmod +x cpr
 sudo mkdir /opt/cpr
 sudo mv cpr /opt/cpr/
-echo "export PATH=\$PATH:/opt/cpr/" >> ~/.bashrc
-exec bash -l
+echo "export PATH=\$PATH:/opt/cpr/" >> ~/.bashrc && exec bash -l
