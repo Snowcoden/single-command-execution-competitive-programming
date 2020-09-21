a43d30cc2ec8442a1c439fe543973d3477978ee7
Arkajyoti Banerjee <34052538+Snowden1738@users.noreply.github.com>
2020-09-21 17:01:51 +0530

    Fixed INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,3 +1,5 @@
+git clone https://github.com/Snowden1738/single-command-execution-competitive-programming
+cd single-command-execution-competitive-programming
 chmod +x cpr
 sudo mkdir /opt/cpr
 sudo mv cpr /opt/cpr/
